eaf3e26c16ad7088677801bb54d9e2eb25e3cbd0
andip71 <andreasp@gmx.de>
2015-08-04 11:31:01 +0200

    cpufreq: Introduce onDemandPlus GOV
    Signed-off-by: Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -392,6 +392,9 @@ extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
 extern struct cpufreq_governor cpufreq_gov_impulse;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDPLUS)
+extern struct cpufreq_governor cpufreq_gov_ondemandplus;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemandplus)
 #endif
 
 
