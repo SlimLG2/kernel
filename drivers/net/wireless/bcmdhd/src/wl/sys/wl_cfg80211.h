d072afd8504343a64710bb8ba7bb53cca7f911cf
Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>
2015-08-30 22:04:44 +0200

    drivers: net: wireless: bcmdhd: Increase scan interval
    Signed-off-by: Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>

--- drivers/net/wireless/bcmdhd/src/wl/sys/wl_cfg80211.h
+++ drivers/net/wireless/bcmdhd/src/wl/sys/wl_cfg80211.h
@@ -184,7 +184,7 @@ do {									\
 #define WL_AF_SEARCH_TIME_MAX           450
 #define WL_AF_TX_EXTRA_TIME_MAX         200
 
-#define WL_SCAN_TIMER_INTERVAL_MS	10000 /* Scan timeout */
+#define WL_SCAN_TIMER_INTERVAL_MS	180000 /* Scan timeout */
 #define WL_CHANNEL_SYNC_RETRY 	5
 #define WL_INVALID 		-1
 
