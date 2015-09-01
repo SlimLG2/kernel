5cbf645b8a502236596456717fd4ea4d7e34f459
Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>
2015-09-02 00:06:36 +0200

    Revert "cpuidle: Check cpuidle_enable_device() return value"
    Introduced bootloop..
    This reverts commit b00fb5f1bd85c404b8ae7da86137354bbdd2e689.

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -337,12 +337,19 @@ int cpuidle_enable_device(struct cpuidle_device *dev)
 
 	if (dev->enabled)
 		return 0;
-
 	if (!drv || !cpuidle_curr_governor)
 		return -EIO;
+	if (!dev->state_count)
+		dev->state_count = drv->state_count;
 
-        if (!dev->registered)
-                return -EINVAL;
+	if (dev->registered == 0) {
+		ret = __cpuidle_register_device(dev);
+		if (ret)
+			return ret;
+	}
+
+	cpuidle_enter_ops = drv->en_core_tk_irqen ?
+		cpuidle_enter_tk : cpuidle_enter;
 
 	poll_idle_init(drv);
 
@@ -464,17 +471,13 @@ int cpuidle_register_device(struct cpuidle_device *dev)
 		return ret;
 	}
 
-	ret = cpuidle_enable_device(dev);
-	if (ret) {
-		mutex_unlock(&cpuidle_lock);
-		return ret;
-	}
-
+	cpuidle_enable_device(dev);
 	cpuidle_install_idle_handler();
 
 	mutex_unlock(&cpuidle_lock);
 
 	return 0;
+
 }
 
 EXPORT_SYMBOL_GPL(cpuidle_register_device);
