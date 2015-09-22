17c6456bbd31e5dcc2df52627514e5996f653136
Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>
2015-09-22 10:25:13 +0200

    cpufreq & d802_defconfig: UberDemand as default GOV

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -404,6 +404,9 @@ extern struct cpufreq_governor cpufreq_gov_ondemandplus;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_UBERDEMAND)
+extern struct cpufreq_governor cpufreq_gov_uberdemand;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_uberdemand)
 #endif
 
 
