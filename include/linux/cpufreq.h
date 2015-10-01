1e7f7c423c1bedd58f6f2128c93ed7b7df79c693
ZaneZam <cyxman@yahoo.com>
2015-10-01 11:53:01 +0200

    cpufreq: Introduce ZZmoove (develop version)
    Signed-off-by: Zdrowy Gosciu <ZdrowyGosciu+GITHUB@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -412,6 +412,9 @@ extern struct cpufreq_governor cpufreq_gov_smartmax;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_UBERDEMAND)
 extern struct cpufreq_governor cpufreq_gov_uberdemand;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_uberdemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
+extern struct cpufreq_governor cpufreq_gov_zzmoove;
+#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
 #endif
 
 
